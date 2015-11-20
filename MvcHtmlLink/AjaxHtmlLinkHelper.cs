a5cfab93f11ddb264296e2ee8d79a04096e9da99
Guillaume Lacasa <guillaumelacasa@hotmail.com>
2015-11-20 16:23:57 +0100

    Initial commit

---
+++ MvcHtmlLink/AjaxHtmlLinkHelper.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using System.Web.Mvc.Ajax;
+using System.Web.Routing;
+using System.Web.WebPages;
+
+namespace MvcHtmlLink
+{
+    public static class AjaxHtmlLinkHelper
+    {
+
+        public static HtmlLink ActionHtmlLink(this AjaxHelper ajaxHelper, string actionName, AjaxOptions ajaxOptions)
+        {
+            return ActionHtmlLink(ajaxHelper, actionName, (string)null /* controllerName */, ajaxOptions);
+        }
+
+        public static HtmlLink ActionHtmlLink(this AjaxHelper ajaxHelper, string actionName, object routeValues, AjaxOptions ajaxOptions)
+        {
+            return ActionHtmlLink(ajaxHelper, actionName, (string)null /* controllerName */, routeValues, ajaxOptions);
+        }
+
+        public static HtmlLink ActionHtmlLink(this AjaxHelper ajaxHelper, string actionName, object routeValues, AjaxOptions ajaxOptions, object htmlAttributes)
+        {
+            return ActionHtmlLink(ajaxHelper, actionName, (string)null /* controllerName */, routeValues, ajaxOptions, htmlAttributes);
+        }
+
+        public static HtmlLink ActionHtmlLink(this AjaxHelper ajaxHelper, string actionName, RouteValueDictionary routeValues, AjaxOptions ajaxOptions)
+        {
+            return ActionHtmlLink(ajaxHelper, actionName, (string)null /* controllerName */, routeValues, ajaxOptions);
+        }
+
+        public static HtmlLink ActionHtmlLink(this AjaxHelper ajaxHelper, string actionName, RouteValueDictionary routeValues, AjaxOptions ajaxOptions, IDictionary<string, object> htmlAttributes)
+        {
+            return ActionHtmlLink(ajaxHelper, actionName, (string)null /* controllerName */, routeValues, ajaxOptions, htmlAttributes);
+        }
+
+        public static HtmlLink ActionHtmlLink(this AjaxHelper ajaxHelper, string actionName, string controllerName, AjaxOptions ajaxOptions)
+        {
+            return ActionHtmlLink(ajaxHelper, actionName, controllerName, null /* values */, ajaxOptions, null /* htmlAttributes */);
+        }
+
+        public static HtmlLink ActionHtmlLink(this AjaxHelper ajaxHelper, string actionName, string controllerName, object routeValues, AjaxOptions ajaxOptions)
+        {
+            return ActionHtmlLink(ajaxHelper, actionName, controllerName, routeValues, ajaxOptions, null /* htmlAttributes */);
+        }
+
+        public static HtmlLink ActionHtmlLink(this AjaxHelper ajaxHelper, string actionName, string controllerName, object routeValues, AjaxOptions ajaxOptions, object htmlAttributes)
+        {
+            RouteValueDictionary newValues = ObjectToDictionary(routeValues);
+            RouteValueDictionary newAttributes = HtmlHelper.AnonymousObjectToHtmlAttributes(htmlAttributes);
+            return ActionHtmlLink(ajaxHelper, actionName, controllerName, newValues, ajaxOptions, newAttributes);
+        }
+
+        public static HtmlLink ActionHtmlLink(this AjaxHelper ajaxHelper, string actionName, string controllerName, RouteValueDictionary routeValues, AjaxOptions ajaxOptions, IDictionary<string, object> htmlAttributes)
+        {
+            return new HtmlLink(ajaxHelper, actionName, controllerName, routeValues, ajaxOptions, htmlAttributes);
+        }
+
+
+
+
+
+        private static RouteValueDictionary ObjectToDictionary(object value)
+        {
+            RouteValueDictionary dictionary = new RouteValueDictionary();
+
+            if (value != null)
+            {
+                foreach (PropertyHelper helper in PropertyHelper.GetProperties(value))
+                {
+                    dictionary.Add(helper.Name, helper.GetValue(value));
+                }
+            }
+
+            return dictionary;
+        }
+    }
+}
---
+++ MvcHtmlLink/HtmlLink.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using System.Web.Mvc.Ajax;
+using System.Web.Routing;
+
+namespace MvcHtmlLink
+{
+    public class HtmlLink : IDisposable
+    {
+        private readonly ViewContext _viewContext;
+
+        public HtmlLink(AjaxHelper ajaxHelper, string actionName, string controllerName, RouteValueDictionary routeValues, AjaxOptions ajaxOptions, IDictionary<string, object> htmlAttributes)
+        {
+            _viewContext = ajaxHelper.ViewContext;
+
+            string targetUrl = UrlHelper.GenerateUrl(null, actionName, controllerName, routeValues, ajaxHelper.RouteCollection, ajaxHelper.ViewContext.RequestContext, true);
+
+            _viewContext.Writer.Write(GenerateLink(ajaxHelper, targetUrl, ajaxOptions, htmlAttributes));
+        }
+
+        private static string GenerateLink(AjaxHelper ajaxHelper, string targetUrl, AjaxOptions ajaxOptions, IDictionary<string, object> htmlAttributes)
+        {
+            TagBuilder tag = new TagBuilder("a");
+
+            tag.MergeAttributes(htmlAttributes);
+            tag.MergeAttribute("href", targetUrl);
+
+            if (ajaxHelper.ViewContext.UnobtrusiveJavaScriptEnabled)
+            {
+                tag.MergeAttributes(ajaxOptions.ToUnobtrusiveHtmlAttributes());
+            }
+            else
+            {
+                throw new NotImplementedException();
+                //tag.MergeAttribute("onclick", GenerateAjaxScript(ajaxOptions, LinkOnClickFormat));
+            }
+
+            return tag.ToString(TagRenderMode.StartTag);
+        }
+
+        public void Dispose()
+        {
+            _viewContext.Writer.Write("</a>");
+        }
+
+    }
+}
