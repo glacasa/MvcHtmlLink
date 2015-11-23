eaf506b08d58795c22ffcf0f58b618e87e93a110
Guillaume Lacasa <guillaumelacasa@hotmail.com>
2015-11-23 11:54:28 +0100

    Helpers renamed to "Begin___Link" to be consistent with "BeginForm"

--- MvcHtmlLink/AjaxHtmlLinkHelper.cs
+++ MvcHtmlLink/AjaxHtmlLinkHelper.cs
@@ -7,49 +7,49 @@ namespace System.Web.Mvc.Ajax
 {
     public static class AjaxHtmlLinkHelper
     {
-        public static HtmlLink ActionHtmlLink(this AjaxHelper ajaxHelper, string actionName, AjaxOptions ajaxOptions)
+        public static HtmlLink BeginActionLink(this AjaxHelper ajaxHelper, string actionName, AjaxOptions ajaxOptions)
         {
-            return ActionHtmlLink(ajaxHelper, actionName, (string)null /* controllerName */, ajaxOptions);
+            return BeginActionLink(ajaxHelper, actionName, (string)null /* controllerName */, ajaxOptions);
         }
 
-        public static HtmlLink ActionHtmlLink(this AjaxHelper ajaxHelper, string actionName, object routeValues, AjaxOptions ajaxOptions)
+        public static HtmlLink BeginActionLink(this AjaxHelper ajaxHelper, string actionName, object routeValues, AjaxOptions ajaxOptions)
         {
-            return ActionHtmlLink(ajaxHelper, actionName, (string)null /* controllerName */, routeValues, ajaxOptions);
+            return BeginActionLink(ajaxHelper, actionName, (string)null /* controllerName */, routeValues, ajaxOptions);
         }
 
-        public static HtmlLink ActionHtmlLink(this AjaxHelper ajaxHelper, string actionName, object routeValues, AjaxOptions ajaxOptions, object htmlAttributes)
+        public static HtmlLink BeginActionLink(this AjaxHelper ajaxHelper, string actionName, object routeValues, AjaxOptions ajaxOptions, object htmlAttributes)
         {
-            return ActionHtmlLink(ajaxHelper, actionName, (string)null /* controllerName */, routeValues, ajaxOptions, htmlAttributes);
+            return BeginActionLink(ajaxHelper, actionName, (string)null /* controllerName */, routeValues, ajaxOptions, htmlAttributes);
         }
 
-        public static HtmlLink ActionHtmlLink(this AjaxHelper ajaxHelper, string actionName, RouteValueDictionary routeValues, AjaxOptions ajaxOptions)
+        public static HtmlLink BeginActionLink(this AjaxHelper ajaxHelper, string actionName, RouteValueDictionary routeValues, AjaxOptions ajaxOptions)
         {
-            return ActionHtmlLink(ajaxHelper, actionName, (string)null /* controllerName */, routeValues, ajaxOptions);
+            return BeginActionLink(ajaxHelper, actionName, (string)null /* controllerName */, routeValues, ajaxOptions);
         }
 
-        public static HtmlLink ActionHtmlLink(this AjaxHelper ajaxHelper, string actionName, RouteValueDictionary routeValues, AjaxOptions ajaxOptions, IDictionary<string, object> htmlAttributes)
+        public static HtmlLink BeginActionLink(this AjaxHelper ajaxHelper, string actionName, RouteValueDictionary routeValues, AjaxOptions ajaxOptions, IDictionary<string, object> htmlAttributes)
         {
-            return ActionHtmlLink(ajaxHelper, actionName, (string)null /* controllerName */, routeValues, ajaxOptions, htmlAttributes);
+            return BeginActionLink(ajaxHelper, actionName, (string)null /* controllerName */, routeValues, ajaxOptions, htmlAttributes);
         }
 
-        public static HtmlLink ActionHtmlLink(this AjaxHelper ajaxHelper, string actionName, string controllerName, AjaxOptions ajaxOptions)
+        public static HtmlLink BeginActionLink(this AjaxHelper ajaxHelper, string actionName, string controllerName, AjaxOptions ajaxOptions)
         {
-            return ActionHtmlLink(ajaxHelper, actionName, controllerName, null /* values */, ajaxOptions, null /* htmlAttributes */);
+            return BeginActionLink(ajaxHelper, actionName, controllerName, null /* values */, ajaxOptions, null /* htmlAttributes */);
         }
 
-        public static HtmlLink ActionHtmlLink(this AjaxHelper ajaxHelper, string actionName, string controllerName, object routeValues, AjaxOptions ajaxOptions)
+        public static HtmlLink BeginActionLink(this AjaxHelper ajaxHelper, string actionName, string controllerName, object routeValues, AjaxOptions ajaxOptions)
         {
-            return ActionHtmlLink(ajaxHelper, actionName, controllerName, routeValues, ajaxOptions, null /* htmlAttributes */);
+            return BeginActionLink(ajaxHelper, actionName, controllerName, routeValues, ajaxOptions, null /* htmlAttributes */);
         }
 
-        public static HtmlLink ActionHtmlLink(this AjaxHelper ajaxHelper, string actionName, string controllerName, object routeValues, AjaxOptions ajaxOptions, object htmlAttributes)
+        public static HtmlLink BeginActionLink(this AjaxHelper ajaxHelper, string actionName, string controllerName, object routeValues, AjaxOptions ajaxOptions, object htmlAttributes)
         {
             RouteValueDictionary newValues = TypeHelper.ObjectToDictionary(routeValues);
             RouteValueDictionary newAttributes = HtmlHelper.AnonymousObjectToHtmlAttributes(htmlAttributes);
-            return ActionHtmlLink(ajaxHelper, actionName, controllerName, newValues, ajaxOptions, newAttributes);
+            return BeginActionLink(ajaxHelper, actionName, controllerName, newValues, ajaxOptions, newAttributes);
         }
 
-        public static HtmlLink ActionHtmlLink(this AjaxHelper ajaxHelper, string actionName, string controllerName, RouteValueDictionary routeValues, AjaxOptions ajaxOptions, IDictionary<string, object> htmlAttributes)
+        public static HtmlLink BeginActionLink(this AjaxHelper ajaxHelper, string actionName, string controllerName, RouteValueDictionary routeValues, AjaxOptions ajaxOptions, IDictionary<string, object> htmlAttributes)
         {
             string targetUrl = UrlHelper.GenerateUrl(null, actionName, controllerName, routeValues, ajaxHelper.RouteCollection, ajaxHelper.ViewContext.RequestContext, true);
             return new HtmlLink(ajaxHelper.ViewContext, targetUrl, htmlAttributes, ajaxOptions);
--- MvcHtmlLink/HtmlLinkHelper.cs
+++ MvcHtmlLink/HtmlLinkHelper.cs
@@ -7,111 +7,111 @@ namespace System.Web.Mvc.Html
 {
     public static class HtmlLinkHelper
     {
-        public static HtmlLink ActionHtmlLink(this HtmlHelper htmlHelper, string actionName)
+        public static HtmlLink BeginActionLink(this HtmlHelper htmlHelper, string actionName)
         {
-            return ActionHtmlLink(htmlHelper, actionName, null /* controllerName */, new RouteValueDictionary(), new RouteValueDictionary());
+            return BeginActionLink(htmlHelper, actionName, null /* controllerName */, new RouteValueDictionary(), new RouteValueDictionary());
         }
 
-        public static HtmlLink ActionHtmlLink(this HtmlHelper htmlHelper, string actionName, object routeValues)
+        public static HtmlLink BeginActionLink(this HtmlHelper htmlHelper, string actionName, object routeValues)
         {
-            return ActionHtmlLink(htmlHelper, actionName, null /* controllerName */, TypeHelper.ObjectToDictionary(routeValues), new RouteValueDictionary());
+            return BeginActionLink(htmlHelper, actionName, null /* controllerName */, TypeHelper.ObjectToDictionary(routeValues), new RouteValueDictionary());
         }
 
-        public static HtmlLink ActionHtmlLink(this HtmlHelper htmlHelper, string actionName, object routeValues, object htmlAttributes)
+        public static HtmlLink BeginActionLink(this HtmlHelper htmlHelper, string actionName, object routeValues, object htmlAttributes)
         {
-            return ActionHtmlLink(htmlHelper, actionName, null /* controllerName */, TypeHelper.ObjectToDictionary(routeValues), HtmlHelper.AnonymousObjectToHtmlAttributes(htmlAttributes));
+            return BeginActionLink(htmlHelper, actionName, null /* controllerName */, TypeHelper.ObjectToDictionary(routeValues), HtmlHelper.AnonymousObjectToHtmlAttributes(htmlAttributes));
         }
 
-        public static HtmlLink ActionHtmlLink(this HtmlHelper htmlHelper, string actionName, RouteValueDictionary routeValues)
+        public static HtmlLink BeginActionLink(this HtmlHelper htmlHelper, string actionName, RouteValueDictionary routeValues)
         {
-            return ActionHtmlLink(htmlHelper, actionName, null /* controllerName */, routeValues, new RouteValueDictionary());
+            return BeginActionLink(htmlHelper, actionName, null /* controllerName */, routeValues, new RouteValueDictionary());
         }
 
-        public static HtmlLink ActionHtmlLink(this HtmlHelper htmlHelper, string actionName, RouteValueDictionary routeValues, IDictionary<string, object> htmlAttributes)
+        public static HtmlLink BeginActionLink(this HtmlHelper htmlHelper, string actionName, RouteValueDictionary routeValues, IDictionary<string, object> htmlAttributes)
         {
-            return ActionHtmlLink(htmlHelper, actionName, null /* controllerName */, routeValues, htmlAttributes);
+            return BeginActionLink(htmlHelper, actionName, null /* controllerName */, routeValues, htmlAttributes);
         }
 
-        public static HtmlLink ActionHtmlLink(this HtmlHelper htmlHelper, string actionName, string controllerName)
+        public static HtmlLink BeginActionLink(this HtmlHelper htmlHelper, string actionName, string controllerName)
         {
-            return ActionHtmlLink(htmlHelper, actionName, controllerName, new RouteValueDictionary(), new RouteValueDictionary());
+            return BeginActionLink(htmlHelper, actionName, controllerName, new RouteValueDictionary(), new RouteValueDictionary());
         }
 
-        public static HtmlLink ActionHtmlLink(this HtmlHelper htmlHelper, string actionName, string controllerName, object routeValues, object htmlAttributes)
+        public static HtmlLink BeginActionLink(this HtmlHelper htmlHelper, string actionName, string controllerName, object routeValues, object htmlAttributes)
         {
-            return ActionHtmlLink(htmlHelper, actionName, controllerName, TypeHelper.ObjectToDictionary(routeValues), HtmlHelper.AnonymousObjectToHtmlAttributes(htmlAttributes));
+            return BeginActionLink(htmlHelper, actionName, controllerName, TypeHelper.ObjectToDictionary(routeValues), HtmlHelper.AnonymousObjectToHtmlAttributes(htmlAttributes));
         }
 
-        public static HtmlLink ActionHtmlLink(this HtmlHelper htmlHelper, string actionName, string controllerName, RouteValueDictionary routeValues, IDictionary<string, object> htmlAttributes)
+        public static HtmlLink BeginActionLink(this HtmlHelper htmlHelper, string actionName, string controllerName, RouteValueDictionary routeValues, IDictionary<string, object> htmlAttributes)
         {
             string targetUrl = UrlHelper.GenerateUrl(null, actionName, controllerName, routeValues, htmlHelper.RouteCollection, htmlHelper.ViewContext.RequestContext, true);
             return new HtmlLink(htmlHelper.ViewContext, targetUrl, htmlAttributes);
         }
 
-        public static HtmlLink ActionHtmlLink(this HtmlHelper htmlHelper, string actionName, string controllerName, string protocol, string hostName, string fragment, object routeValues, object htmlAttributes)
+        public static HtmlLink BeginActionLink(this HtmlHelper htmlHelper, string actionName, string controllerName, string protocol, string hostName, string fragment, object routeValues, object htmlAttributes)
         {
-            return ActionHtmlLink(htmlHelper, actionName, controllerName, protocol, hostName, fragment, TypeHelper.ObjectToDictionary(routeValues), HtmlHelper.AnonymousObjectToHtmlAttributes(htmlAttributes));
+            return BeginActionLink(htmlHelper, actionName, controllerName, protocol, hostName, fragment, TypeHelper.ObjectToDictionary(routeValues), HtmlHelper.AnonymousObjectToHtmlAttributes(htmlAttributes));
         }
 
-        public static HtmlLink ActionHtmlLink(this HtmlHelper htmlHelper, string actionName, string controllerName, string protocol, string hostName, string fragment, RouteValueDictionary routeValues, IDictionary<string, object> htmlAttributes)
+        public static HtmlLink BeginActionLink(this HtmlHelper htmlHelper, string actionName, string controllerName, string protocol, string hostName, string fragment, RouteValueDictionary routeValues, IDictionary<string, object> htmlAttributes)
         {
             string targetUrl = UrlHelper.GenerateUrl(null, actionName, controllerName, protocol, hostName, fragment, routeValues, htmlHelper.RouteCollection, htmlHelper.ViewContext.RequestContext, true);
             return new HtmlLink(htmlHelper.ViewContext, targetUrl, htmlAttributes);
         }
 
 
-        public static HtmlLink RouteHtmlLink(this HtmlHelper htmlHelper, object routeValues)
+        public static HtmlLink BeginRouteLink(this HtmlHelper htmlHelper, object routeValues)
         {
-            return RouteHtmlLink(htmlHelper, TypeHelper.ObjectToDictionary(routeValues));
+            return BeginRouteLink(htmlHelper, TypeHelper.ObjectToDictionary(routeValues));
         }
 
-        public static HtmlLink RouteHtmlLink(this HtmlHelper htmlHelper, RouteValueDictionary routeValues)
+        public static HtmlLink BeginRouteLink(this HtmlHelper htmlHelper, RouteValueDictionary routeValues)
         {
-            return RouteHtmlLink(htmlHelper, routeValues, new RouteValueDictionary());
+            return BeginRouteLink(htmlHelper, routeValues, new RouteValueDictionary());
         }
 
-        public static HtmlLink RouteHtmlLink(this HtmlHelper htmlHelper,  string routeName)
+        public static HtmlLink BeginRouteLink(this HtmlHelper htmlHelper,  string routeName)
         {
-            return RouteHtmlLink(htmlHelper,  routeName, (object)null /* routeValues */);
+            return BeginRouteLink(htmlHelper,  routeName, (object)null /* routeValues */);
         }
 
-        public static HtmlLink RouteHtmlLink(this HtmlHelper htmlHelper,  string routeName, object routeValues)
+        public static HtmlLink BeginRouteLink(this HtmlHelper htmlHelper,  string routeName, object routeValues)
         {
-            return RouteHtmlLink(htmlHelper,  routeName, TypeHelper.ObjectToDictionary(routeValues));
+            return BeginRouteLink(htmlHelper,  routeName, TypeHelper.ObjectToDictionary(routeValues));
         }
 
-        public static HtmlLink RouteHtmlLink(this HtmlHelper htmlHelper,  string routeName, RouteValueDictionary routeValues)
+        public static HtmlLink BeginRouteLink(this HtmlHelper htmlHelper,  string routeName, RouteValueDictionary routeValues)
         {
-            return RouteHtmlLink(htmlHelper,  routeName, routeValues, new RouteValueDictionary());
+            return BeginRouteLink(htmlHelper,  routeName, routeValues, new RouteValueDictionary());
         }
 
-        public static HtmlLink RouteHtmlLink(this HtmlHelper htmlHelper,  object routeValues, object htmlAttributes)
+        public static HtmlLink BeginRouteLink(this HtmlHelper htmlHelper,  object routeValues, object htmlAttributes)
         {
-            return RouteHtmlLink(htmlHelper,  TypeHelper.ObjectToDictionary(routeValues), HtmlHelper.AnonymousObjectToHtmlAttributes(htmlAttributes));
+            return BeginRouteLink(htmlHelper,  TypeHelper.ObjectToDictionary(routeValues), HtmlHelper.AnonymousObjectToHtmlAttributes(htmlAttributes));
         }
 
-        public static HtmlLink RouteHtmlLink(this HtmlHelper htmlHelper,  RouteValueDictionary routeValues, IDictionary<string, object> htmlAttributes)
+        public static HtmlLink BeginRouteLink(this HtmlHelper htmlHelper,  RouteValueDictionary routeValues, IDictionary<string, object> htmlAttributes)
         {
-            return RouteHtmlLink(htmlHelper,  null /* routeName */, routeValues, htmlAttributes);
+            return BeginRouteLink(htmlHelper,  null /* routeName */, routeValues, htmlAttributes);
         }
 
-        public static HtmlLink RouteHtmlLink(this HtmlHelper htmlHelper,  string routeName, object routeValues, object htmlAttributes)
+        public static HtmlLink BeginRouteLink(this HtmlHelper htmlHelper,  string routeName, object routeValues, object htmlAttributes)
         {
-            return RouteHtmlLink(htmlHelper,  routeName, TypeHelper.ObjectToDictionary(routeValues), HtmlHelper.AnonymousObjectToHtmlAttributes(htmlAttributes));
+            return BeginRouteLink(htmlHelper,  routeName, TypeHelper.ObjectToDictionary(routeValues), HtmlHelper.AnonymousObjectToHtmlAttributes(htmlAttributes));
         }
 
-        public static HtmlLink RouteHtmlLink(this HtmlHelper htmlHelper,  string routeName, RouteValueDictionary routeValues, IDictionary<string, object> htmlAttributes)
+        public static HtmlLink BeginRouteLink(this HtmlHelper htmlHelper,  string routeName, RouteValueDictionary routeValues, IDictionary<string, object> htmlAttributes)
         {
             string targetUrl = UrlHelper.GenerateUrl(routeName, null, null, routeValues, htmlHelper.RouteCollection, htmlHelper.ViewContext.RequestContext, true);
             return new HtmlLink(htmlHelper.ViewContext, targetUrl, htmlAttributes);
         }
 
-        public static HtmlLink RouteHtmlLink(this HtmlHelper htmlHelper,  string routeName, string protocol, string hostName, string fragment, object routeValues, object htmlAttributes)
+        public static HtmlLink BeginRouteLink(this HtmlHelper htmlHelper,  string routeName, string protocol, string hostName, string fragment, object routeValues, object htmlAttributes)
         {
-            return RouteHtmlLink(htmlHelper,  routeName, protocol, hostName, fragment, TypeHelper.ObjectToDictionary(routeValues), HtmlHelper.AnonymousObjectToHtmlAttributes(htmlAttributes));
+            return BeginRouteLink(htmlHelper,  routeName, protocol, hostName, fragment, TypeHelper.ObjectToDictionary(routeValues), HtmlHelper.AnonymousObjectToHtmlAttributes(htmlAttributes));
         }
 
-        public static HtmlLink RouteHtmlLink(this HtmlHelper htmlHelper,  string routeName, string protocol, string hostName, string fragment, RouteValueDictionary routeValues, IDictionary<string, object> htmlAttributes)
+        public static HtmlLink BeginRouteLink(this HtmlHelper htmlHelper,  string routeName, string protocol, string hostName, string fragment, RouteValueDictionary routeValues, IDictionary<string, object> htmlAttributes)
         {
             string targetUrl = UrlHelper.GenerateUrl(routeName, null, null, protocol, hostName, fragment, routeValues, htmlHelper.RouteCollection, htmlHelper.ViewContext.RequestContext, true);
             return new HtmlLink(htmlHelper.ViewContext, targetUrl, htmlAttributes);
