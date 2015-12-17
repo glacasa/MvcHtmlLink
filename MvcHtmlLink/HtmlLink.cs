e0f125f32926f7098e5c2a784e0deb2e1593538f
Guillaume Lacasa <guillaumelacasa@hotmail.com>
2015-12-17 18:30:13 +0100

    Ajax link now works with UnobtrusiveJavaScriptEnabled=false

--- MvcHtmlLink/HtmlLink.cs
+++ MvcHtmlLink/HtmlLink.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using MvcHtmlLink.System.Web.WebPages;
+using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +13,10 @@ namespace MvcHtmlLink
 {
     public class HtmlLink : IDisposable
     {
+        private const string LinkOnClickFormat = "Sys.Mvc.AsyncHyperlink.handleClick(this, new Sys.UI.DomEvent(event), {0});";
+        private const string FormOnClickValue = "Sys.Mvc.AsyncForm.handleClick(this, new Sys.UI.DomEvent(event));";
+        private const string FormOnSubmitFormat = "Sys.Mvc.AsyncForm.handleSubmit(this, new Sys.UI.DomEvent(event), {0});";
+
         private readonly ViewContext _viewContext;
 
         public HtmlLink(ViewContext viewContext, string targetUrl, IDictionary<string, object> htmlAttributes, AjaxOptions ajaxOptions = null)
@@ -19,7 +25,7 @@ namespace MvcHtmlLink
             _viewContext.Writer.Write(GenerateLink(targetUrl, htmlAttributes, ajaxOptions));
         }
         
-        private static string GenerateLink(string targetUrl, IDictionary<string, object> htmlAttributes, AjaxOptions ajaxOptions)
+        private string GenerateLink(string targetUrl, IDictionary<string, object> htmlAttributes, AjaxOptions ajaxOptions)
         {
             TagBuilder tag = new TagBuilder("a");
 
@@ -28,10 +34,26 @@ namespace MvcHtmlLink
 
             if (ajaxOptions != null)
             {
-                tag.MergeAttributes(ajaxOptions.ToUnobtrusiveHtmlAttributes());
+                if (_viewContext.UnobtrusiveJavaScriptEnabled)
+                {
+                    tag.MergeAttributes(ajaxOptions.ToUnobtrusiveHtmlAttributes());
+                }
+                else
+                {
+                    tag.MergeAttribute("onclick", GenerateAjaxScript(ajaxOptions, LinkOnClickFormat));
+                }
+                //tag.MergeAttributes(ajaxOptions.ToUnobtrusiveHtmlAttributes());
             }
 
             return tag.ToString(TagRenderMode.StartTag);
+        }
+
+        private static string GenerateAjaxScript(AjaxOptions ajaxOptions, string scriptFormat)
+        {
+            var ajaxOptionsHelper = new AjaxOptionsHelper(ajaxOptions);
+            string optionsString = ajaxOptionsHelper.ToJavascriptString();
+
+            return String.Format(CultureInfo.InvariantCulture, scriptFormat, optionsString);
         }
 
 
---
+++ MvcHtmlLink/System.Web.WebPages/AjaxOptionsHelper.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc.Ajax;
+
+namespace MvcHtmlLink.System.Web.WebPages
+{
+    internal class AjaxOptionsHelper
+    {
+        private AjaxOptions AjaxOptions { get; set; }
+
+        internal AjaxOptionsHelper(AjaxOptions ajaxOptions)
+        {
+            this.AjaxOptions = ajaxOptions;
+        }
+
+        internal string InsertionModeString
+        {
+            get
+            {
+                switch (AjaxOptions.InsertionMode)
+                {
+                    case InsertionMode.Replace:
+                        return "Sys.Mvc.InsertionMode.replace";
+                    case InsertionMode.InsertBefore:
+                        return "Sys.Mvc.InsertionMode.insertBefore";
+                    case InsertionMode.InsertAfter:
+                        return "Sys.Mvc.InsertionMode.insertAfter";
+                    default:
+                        return ((int)AjaxOptions.InsertionMode).ToString(CultureInfo.InvariantCulture);
+                }
+            }
+        }
+
+        private static string PropertyStringIfSpecified(string propertyName, string propertyValue)
+        {
+            if (!String.IsNullOrEmpty(propertyValue))
+            {
+                string escapedPropertyValue = propertyValue.Replace("'", @"\'");
+                return String.Format(CultureInfo.InvariantCulture, " {0}: '{1}',", propertyName, escapedPropertyValue);
+            }
+            return String.Empty;
+        }
+
+        private static string EventStringIfSpecified(string propertyName, string handler)
+        {
+            if (!String.IsNullOrEmpty(handler))
+            {
+                return String.Format(CultureInfo.InvariantCulture, " {0}: Function.createDelegate(this, {1}),", propertyName, handler.ToString());
+            }
+            return String.Empty;
+        }
+
+        internal string ToJavascriptString()
+        {
+            // creates a string of the form { key1: value1, key2 : value2, ... }
+            // This method is used for generating obtrusive JavaScript (using MicrosoftMvcAjax.js) which is no longer 
+            // actively maintained. Consequently, we'll ignore the AllowCache option if it's set for this code path.
+            StringBuilder optionsBuilder = new StringBuilder("{");
+            optionsBuilder.AppendFormat(CultureInfo.InvariantCulture, " insertionMode: {0},", InsertionModeString);
+            optionsBuilder.Append(PropertyStringIfSpecified("confirm", AjaxOptions.Confirm));
+            optionsBuilder.Append(PropertyStringIfSpecified("httpMethod", AjaxOptions.HttpMethod));
+            optionsBuilder.Append(PropertyStringIfSpecified("loadingElementId", AjaxOptions.LoadingElementId));
+            optionsBuilder.Append(PropertyStringIfSpecified("updateTargetId", AjaxOptions.UpdateTargetId));
+            optionsBuilder.Append(PropertyStringIfSpecified("url", AjaxOptions.Url));
+            optionsBuilder.Append(EventStringIfSpecified("onBegin", AjaxOptions.OnBegin));
+            optionsBuilder.Append(EventStringIfSpecified("onComplete", AjaxOptions.OnComplete));
+            optionsBuilder.Append(EventStringIfSpecified("onFailure", AjaxOptions.OnFailure));
+            optionsBuilder.Append(EventStringIfSpecified("onSuccess", AjaxOptions.OnSuccess));
+            optionsBuilder.Length--;
+            optionsBuilder.Append(" }");
+            return optionsBuilder.ToString();
+        }
+    }
+}
